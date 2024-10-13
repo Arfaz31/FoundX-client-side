a73523f72fc35cdb517c791f076afc211c076eac
Arfaz31 <arfazahamed31@gmail.com>
2024-10-13 18:11:42 +0600

    set group layout and make-skeleton for loading and  create-NavbarDropdown

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/order */
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -7,40 +8,16 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
-
 import { siteConfig } from "@/src/config/site";
-import { ThemeSwitch } from "@/src/components/ui/theme-switch";
-import { SearchIcon, Logo } from "@/src/components/icons";
+import { Logo } from "../icons";
+import { ThemeSwitch } from "./theme-switch";
+import NavbarDropdown from "./NavbarDropdown";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -74,6 +51,9 @@ export const Navbar = () => {
       >
         <NavbarItem className="hidden sm:flex gap-2">
           <ThemeSwitch />
+        </NavbarItem>
+        <NavbarItem className="hidden sm:flex gap-2">
+          <NavbarDropdown />
         </NavbarItem>
       </NavbarContent>
 
